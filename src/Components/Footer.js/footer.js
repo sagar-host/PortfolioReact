0e91acc47701521661bbb2ab711a7f34801bfb24
sagar <rebelsagarx@gmail.com>
2022-03-26 19:16:14 +0530

    first

--- src/Components/Footer.js/footer.js
+++ src/Components/Footer.js/footer.js
@@ -2,32 +2,34 @@ import React from 'react'
 import "./Footer.css";
 import 'font-awesome/css/font-awesome.min.css';
 
+import {Link} from "react-router-dom"
+
 function footer() {
   return (
    
   <footer id='mainFooter'>
 <div className="footer-menu">
  <ul className="f-menu">
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
-    <li><a href="/blog">Blog</a></li>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">About</Link></li>
+    <li><Link to="/">Contact</Link></li>
+    <li><Link to="/">Blog</Link></li>
  </ul>
 </div>
 
 <div className="icons">
 <ul className="socials">
-<li><a href="https://www.github.com/sagar-host"><i className="fa fa-github"></i></a></li>
-<li><a href="/"><i className="fa fa-twitter"></i></a></li>
-<li><a href="/"><i className="fa fa-google-plus"></i></a></li>
-<li><a href="/"><i className="fa fa-youtube"></i></a></li>
-<li><a href="https://www.linkedin.com/in/sagar-208723153"><i className="fa fa-linkedin-square"></i></a></li>
+<li><Link to="https://www.github.com/sagar-host"><i className="fa fa-github"></i></Link></li>
+<li><Link to="/"><i className="fa fa-twitter"></i></Link></li>
+<li><Link to="/"><i className="fa fa-google-plus"></i></Link></li>
+<li><Link to="/"><i className="fa fa-youtube"></i></Link></li>
+<li><Link to="https://www.linkedin.com/in/sagar-208723153"><i className="fa fa-linkedin-square"></i></Link></li>
 </ul>
 </div>
 
 <div className="footer-bottom">
 <h3 style={{fontFamily:"'Brittany Signature', sans-serif"}}>Sagar.Bleu</h3>
-<p>copyright &copy;2021 <a href="/">Sagar</a>  </p>
+<p>copyright &copy;2021 <Link to="/">Sagar</Link>  </p>
 </div>
 
 
